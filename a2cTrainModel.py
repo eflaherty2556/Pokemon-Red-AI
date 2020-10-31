0c0a3ca8fafdc37f856714b9e773078641d57e7e
Aaron Jimenez <roceaaan@gmail.com>
2020-10-31 16:50:58 -0700

    Added a custom policy

--- a2cTrainModel.py
+++ a2cTrainModel.py
@@ -2,17 +2,27 @@ import retro
 import gym
 import os
 import time
+import retro.enums
 
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines import A2C
 from stable_baselines.common.env_checker import check_env
 from stable_baselines.common.cmd_util import make_vec_env
+from stable_baselines.common.policies import FeedForwardPolicy, register_policy
+
 
 # party size = d163
 # money = d347
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class CustomPolicy(FeedForwardPolicy):
+    def __init__(self, *args, **kwargs):
+        super(CustomPolicy, self).__init__(*args, **kwargs,
+                                           net_arch=[dict(pi=[128, 128, 128],
+                                                          vf=[128, 128, 128])],
+                                           feature_extraction="mlp")
 
 def train_model(n_vec = 4, time_steps = 2000):
         retro.data.Integrations.add_custom_path(
@@ -27,7 +37,7 @@ def train_model(n_vec = 4, time_steps = 2000):
         vec_env = make_vec_env(lambda: env, n_envs=n_vec)
         # time.sleep(3)    
 
-        model = A2C(MlpPolicy, vec_env, verbose=1)
+        model = A2C(CustomPolicy, vec_env, verbose=1)
 
         start_time = time.time()
         model.learn(total_timesteps=time_steps)
