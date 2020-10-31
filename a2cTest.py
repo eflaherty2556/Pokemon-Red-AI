76a082b2c4ccb60c8ca2858cd3f2c72174af0b16
JFlaherty347 <Jflaherty347@gmail.com>
2020-10-31 16:45:11 -0700

    increased neural network size for a2c(3x128)

--- a2cTest.py
+++ a2cTest.py
@@ -9,9 +9,15 @@ from stable_baselines.common.policies import MlpPolicy
 from stable_baselines import A2C
 from stable_baselines.common.env_checker import check_env
 from stable_baselines.common.cmd_util import make_vec_env
+from stable_baselines.common.policies import FeedForwardPolicy, register_policy
 
-# party size = d163
-# money = d347
+#
+class CustomPolicy(FeedForwardPolicy):
+    def __init__(self, *args, **kwargs):
+        super(CustomPolicy, self).__init__(*args, **kwargs,
+                                           net_arch=[dict(pi=[128, 128, 128],
+                                                          vf=[128, 128, 128])],
+                                           feature_extraction="mlp")
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -28,10 +34,10 @@ def main():
         vec_env = make_vec_env(lambda: env, n_envs=4)
         # time.sleep(3)    
 
-        model = A2C(MlpPolicy, vec_env, verbose=1)
+        model = A2C(CustomPolicy, vec_env, verbose=1)
 
         start_time = time.time()
-        model.learn(total_timesteps=200000)
+        model.learn(total_timesteps=50000)
         print("TRAINING COMPLETE! Time elapsed: ", str(time.time()-start_time))
         
         print("Attempting to get first pokemon!")
