7f17e4b87dcaa3594c5b4d17c892669378658ac9
JFlaherty347 <Jflaherty347@gmail.com>
2021-01-12 15:11:41 -0800

    created seperate evaluator script to run for set timesteps

--- custom_integrations/PokemonRed-GameBoy/reward.lua
+++ custom_integrations/PokemonRed-GameBoy/reward.lua
@@ -16,8 +16,8 @@ function done_check()
 	-- if data.hasOaksParcel > 0 then
 	if data.party_size >= 1 then
 		return true
-	elseif frame_count > MAX_RUN_FRAMES then
-		return true
+	-- elseif frame_count > MAX_RUN_FRAMES then
+	-- 	return true
 	end
 
 	return false
